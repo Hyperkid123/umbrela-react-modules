0098c21b8763e5074ec31a0b8b70f63d56e2f0d5
Martin Marosi <marvusm.mmi@gmail.com>
2018-03-21 13:00:32 +0100

    SheetFill component test

--- config/setupTest.js
+++ config/setupTest.js
@@ -13,6 +13,11 @@ global.mount = mount;
 global.toJson = toJson;
 global.window.base = 'http://localhost:3000/';
 global.window.researchId = 1;
+//setup for testing scroll to DOM element in sheetFill.test.js
+const questionTestElement = global.window.document.createElement('div');
+questionTestElement.setAttribute('id', 'question_99');
+questionTestElement.scrollIntoView = jest.fn();
+global.window.document.getElementsByTagName('body')[0].appendChild(questionTestElement);
 
 // Fail tests on any warning
 console.error = message => {
